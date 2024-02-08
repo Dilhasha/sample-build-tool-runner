01f7403d81eec5d9db47952d1bf3cdeb988b8726
ShammiL <shammi0107@gmail.com>
2024-02-08 09:43:26 +0530

    Change package for interface

--- src/main/java/build/tools/runners/OpenAPIToolRunner.java
+++ src/main/java/build/tools/runners/OpenAPIToolRunner.java
@@ -1,7 +1,7 @@
 package build.tools.runners;
 
-import io.ballerina.cli.tool.CodeGeneratorTool;
-import io.ballerina.projects.ToolContext;
+import io.ballerina.projects.buildtools.CodeGeneratorTool;
+import io.ballerina.projects.buildtools.ToolContext;
 
 public class OpenAPIToolRunner implements CodeGeneratorTool {
     @Override
@@ -10,7 +10,7 @@ public class OpenAPIToolRunner implements CodeGeneratorTool {
         System.out.println("toolContext.toolId() = " + toolContext.toolId());
         System.out.println("toolContext.sourceValue() = " + toolContext.filePath());
         System.out.println("toolContext.targetModule() = " + toolContext.targetModule());
-        System.out.println("toolContext.optionsTable() = " + toolContext.optionsTable());
+        System.out.println("toolContext.optionsTable() = " + toolContext.options());
         System.out.println("toolContext.cachePath() = " + toolContext.cachePath());
         System.out.println("toolContext.outputPath() = " + toolContext.outputPath());
     }
